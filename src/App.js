399d857f7e4c0ca3c3b62edceb3e5b3e6a28d5e4
Chris Casey <cdcasey@users.noreply.github.com>
2023-06-26 14:37:26 -0500

    Sample app in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Collapse from '@mui/material/Collapse';
+
 import './App.css';
 
+const cardContent = [1, 2, 3, 4, 5, 6]
+const firstThreeCardContent = cardContent.slice(0, 3)
+const restOfCardContent = cardContent.slice(3)
+
 function App() {
+  const [collapsed, setCollapsed] = React.useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="cardGrid">
+        {firstThreeCardContent.map((item, index) => (
+          <Card sx={{ minWidth: 275 }} key={index}>
+            <CardContent>
+              <h1>Card {item}</h1>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <Collapse in={collapsed}>
+        <div className="cardGrid">
+          {restOfCardContent.map((item, index) => (
+            <Card sx={{ minWidth: 275 }} key={index}>
+              <CardContent>
+                <h1>Card {item}</h1>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </Collapse>
+
+
+      <Button variant="contained" onClick={() => setCollapsed(!collapsed)}>Click Me</Button>
+    </>
   );
 }
 
